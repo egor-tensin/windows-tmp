e54523199b6cbc36d49523ffd66b0d695cd0b755
Egor Tensin <Egor.Tensin@gmail.com>
2017-01-15 23:36:26 +0300

    bugfix
    * Add missing '--'s.

--- fix_nt_symbol_path.sh
+++ fix_nt_symbol_path.sh
@@ -17,8 +17,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-readonly script_argv0="$( printf '%q' "${BASH_SOURCE[0]}" )"
-readonly script_dir="$( cd "$( dirname -- "$script_argv0" )" && pwd )"
+readonly script_argv0="$( printf -- '%q' "${BASH_SOURCE[0]}" )"
+readonly script_dir="$( cd -- "$( dirname -- "$script_argv0" )" && pwd )"
 
 dump() {
     local prefix="${FUNCNAME[0]}"
